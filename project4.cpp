b0ebd8ffeea7bf06cfddb37e7fa72fd65cb62949
Amirhosseinhabibi <117579539+Amirhosseinhabibi@users.noreply.github.com>
2022-11-06 18:55:21 +0330

    Add files via upload

---
+++ project4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main() {
+int a=0.5,b=0.75,c;
+cin >>a>>b;
+c=a+b;
+cout <<c;
+c=a-b;
+cout <<c;
+c=a*b;
+cout<<c;
+c=a%b;
+cout <<c;
+}
